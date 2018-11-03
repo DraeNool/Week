b1630cda7ba89d8acf6998fe0f2191b5492e8959
Leonardo Torres <mcmgrow@gmail.com>
2018-11-03 18:21:46 -0500

    Commit

---
+++ app/src/main/java/com/leonardo/week/DeteccionObjetosActivity.java
@@ -0,0 +1,13 @@
+package com.leonardo.week;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeteccionObjetosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deteccion_objetos);
+    }
+}
