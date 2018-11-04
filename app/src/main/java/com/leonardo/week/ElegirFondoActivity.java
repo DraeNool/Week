30028b710494967763f854396cd7d5a486029ba0
Leonardo Torres <mcmgrow@gmail.com>
2018-11-03 19:00:26 -0500

    Commit

--- app/src/main/java/com/leonardo/week/ElegirFondoActivity.java
+++ app/src/main/java/com/leonardo/week/ElegirFondoActivity.java
@@ -3,11 +3,11 @@ package com.leonardo.week;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CambioFondoActivity extends AppCompatActivity {
+public class ElegirFondoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_capturar_fondo);
+        setContentView(R.layout.activity_elegir_fondo);
     }
 }
